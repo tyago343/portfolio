59c5c49d39f627ee3dafcb0cdc1e7fedbae5e902
tyago343 <tyagocasanova@gmail.com>
2020-03-09 22:27:55 +0100

    cambios en las rutas de react router

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,26 +1,31 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Fragment } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import Signup from "./Signup/index";
 import Login from "./Login/index";
-import Games from "./Games/index";
 import NoMatch from "./NoMatch/index";
+import TicTacToe from "./Games/TicTacToe";
 
 const Main = () => {
   return (
-    <Switch>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/games">
-        <Games />
-      </Route>
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </Switch>
+    <Fragment>
+      <Link to="/signup">Registro</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/games/tictac">TaTeTi</Link>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/games/tictac">
+          <TicTacToe />
+        </Route>
+        <Route path="/*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 };
 export default Main;
