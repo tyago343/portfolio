1c2da60ed7f4847f48194d9266f7e59afd61ea5c
tyago343 <tyagocasanova@gmail.com>
2020-03-30 18:38:04 +0200

    La idea es comenzar a utilizar la camara

--- frontend/src/pages/Games/TicTacToe/index.js
+++ frontend/src/pages/Games/TicTacToe/index.js
@@ -31,15 +31,15 @@ const TicTacToe = props => {
       <H2>Bienvenido al TaTeTi</H2>
       <P>{winner || ""}</P>
       <Ul>
-        <Li data-index="1" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="2" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="3" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="4" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="5" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="6" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="7" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="8" data-status="free" onClick={handleClick}></Li>
-        <Li data-index="9" data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
+        <Li data-status="free" onClick={handleClick}></Li>
       </Ul>
       <Span onClick={resetButton}>Reiniciar Juego</Span>
     </div>
---
+++ frontend/src/pages/Utilities/PhotoCam/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PhotoCam = () => {
+  return (
+    <div>
+      <p>
+        Esta pagina tendrá una funcionalidad para sacarse fotos. Ahora es una
+        practica pero la intencion es que los usuarios se puedan sacar fotos de
+        perfil
+      </p>
+    </div>
+  );
+};
+export default PhotoCam;
