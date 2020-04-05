65c717af11f3a9228fe2fed7cdf10a440d651493
tyago343 <tyagocasanova@gmail.com>
2020-04-05 13:56:36 +0200

    cambio de libreria

--- frontend/src/pages/Utilities/PhotoCam/index.js
+++ frontend/src/pages/Utilities/PhotoCam/index.js
@@ -1,29 +1,35 @@
 import React, { useState } from "react";
-import Camera from "react-html5-camera-photo";
+import Webcam from "react-webcam";
 
-import ImagePreview from "./ImagePreview";
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
 
-function PhotoCam(props) {
-  const [dataUri, setDataUri] = useState("");
+const WebcamCapture = () => {
+  const [pic, setPic] = useState(null);
+  const webcamRef = React.useRef(null);
 
-  function handleTakePhotoAnimationDone(dataUri) {
-    console.log("takePhoto");
-    setDataUri(dataUri);
-  }
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setPic(imageSrc);
+  }, [webcamRef]);
 
-  const isFullscreen = false;
   return (
-    <div>
-      {dataUri ? (
-        <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} />
-      ) : (
-        <Camera
-          onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
-          isFullscreen={isFullscreen}
-        />
-      )}
-    </div>
+    <>
+      <Webcam
+        audio={false}
+        height={400}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={600}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+      {pic ? <img src={pic} tab="picture from app" /> : ""}
+    </>
   );
-}
+};
 
-export default PhotoCam;
+export default WebcamCapture;
