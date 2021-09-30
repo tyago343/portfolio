ed9e684ae273c2e5e134edb10afa9c7fac412313
tyago343 <tyagocasanova@gmail.com>
2021-09-30 12:14:42 +0200

    Initial work for frontend

---
+++ frontend/src/pages/Homepage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+  return (
+    <main>
+      <div>
+        <h1>Santiago Casanova -- Frontend developer</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Homepage;
